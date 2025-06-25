ddc381711f6824f9d568801d9cf5f095a315770d
SURAJ045 <surajmp045@gmail.com>
2025-06-25 23:31:38 +0530

    fetching data

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const button = ddocument.getElementById('search-button');
 const city = document.getElementById('search-input');
 
-async function fetchdata(){
-    fetch(`http://api.weatherapi.com/v1/current.json?key=e83fd402afd24316a89185311252106&q=$(cityName)&aqi=yes`)
+async function fetchdata(CityName){
+    
+    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=e83fd402afd24316a89185311252106&q=${cityName}&aqi=yes`)
+    return await promise.json()
 }
 
 
 button.addEventListener('click', () => { 
-    
-
+    const value = input.value
+    const result = getData(value)
+    console.log(result)
 })
